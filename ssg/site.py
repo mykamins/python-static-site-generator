e614525ea140080cebc4db06a98a0a2cdd1f0f26
mykamins <mykamins@gmail.com>
2022-12-01 11:37:20 +0200

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -12,5 +12,5 @@ class Site:
     def build(self):
         self.dest.mkdir(parents = True, exist_ok = True)
         for path in self.source.rglob("*"):
-            if self.source.isdir(path):
+            if path.is_dir():
                 create_dir(path)
